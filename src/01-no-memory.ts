ca9e005a915f199d236f11253e00895c1202c286
Giga Mania <hil.giga@gmail.com>
2025-09-15 22:45:59 +0100

    no-memory

---
+++ src/01-no-memory.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import { OpenAI } from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+let response = await openai.responses.create({
+  model: "gpt-4o-mini",
+  input: [
+    {
+      role: "system",
+      content:
+        "You are a helpful assistant that can answer questions and help with tasks. Your name is Cudo.",
+    },
+    {
+      role: "user",
+      content: "Hey! I'm a Giga! What's your name?",
+    },
+  ],
+});
+
+response = await openai.responses.create({
+  model: "gpt-4o-mini",
+  input: [
+    {
+      role: "user",
+      content: "Hey! What's my name? What's your name?",
+    },
+  ],
+});
+
+console.log(response.output_text);
