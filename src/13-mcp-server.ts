bd6dc72da9b79336f8d1c18636992df562cfc68b
Giga Mania <hil.giga@gmail.com>
2025-09-17 16:19:33 +0100

    mcp server/client

---
+++ src/13-mcp-server.ts
@@ -0,0 +1,75 @@
+import * as fs from "node:fs/promises";
+import {z} from "zod";
+
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+
+// mcp-server is the program that provides standardized interface to expose tools to ai applications.
+// this ai applications called mcp-hosts and they implement mcp-client to interact with the mcp-server.
+// mcp-hosts/ai-applications initialise the mcp-client to communicate with the mcp-server.
+// each mcp-host/ai-application can have multiple mcp-clients. and each client only handles one mcp-server.
+
+
+const server = new McpServer({
+  name: "local-fs",
+  version: "1.0.0",
+  description: "Local file system MCP server",
+});
+
+server.registerTool(
+  "list_files",
+  {
+    title: "List Files",
+    description: "List files and directories at a given path",
+    inputSchema: {
+      path: z
+        .string()
+        .optional()
+        .describe(
+          "Path to directory to list content of it (defaults to current directory)"
+        ),
+    },
+  },
+  async ({ path }) => {
+    try {
+      const content = await fs.readdir(path || "./", { withFileTypes: true });
+      const contentList = content.map((c) => ({
+        name: c.name,
+        isDirectory: c.isDirectory(),
+        isFile: c.isFile(),
+        isSymbolicLink: c.isSymbolicLink(),
+      }));
+      return {
+        content: [
+          {
+            type: "text",
+            text: JSON.stringify(contentList, null, 2),
+          },
+        ],
+      };
+    } catch (error) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Error listing files: ${
+              error instanceof Error ? error.message : String(error)
+            }`,
+          },
+        ],
+      };
+    }
+  }
+);
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("fs MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
---
+++ src/14-mcp-client.ts
@@ -0,0 +1,39 @@
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+
+
+// mcp-client is the program that implements the mcp-client interface to communicate with the mcp-server.
+// each mcp-client only hanldes communication with only one mcp-server.
+
+// but why would I use mcp, and build mcp servers and clients? how that helps?
+// I want to build a program (mcp-server) that will allow me and others to use the tools that are exposed by the mcp-server.
+// And want to do it once and use it in multiple places.
+// so, after others can have their own mcp-client to be connect to my mcp-server, see what avaialble and use them as needed.
+
+
+// in this example mcp-client connect to local mcp-server
+// clients 
+
+async function main() {
+  const transport = new StdioClientTransport({
+    command: "node",
+    args: ["build/13-mcp-server.cjs"],
+  });
+  const client = new Client({name: "local-fs-client", version: "1.0.0"});
+
+  await client.connect(transport);
+
+  console.log("Connected to local-fs MCP server");
+
+  const response = await client.listTools();
+  console.log("Tools:", response);
+
+  console.log("================================================")
+
+  const toolCallResult = await client.callTool({ name: "list_files", arguments: { path: "./" } });
+  console.log("Tool call:", toolCallResult);
+
+  await client.close();
+}
+
+main().catch((err) => console.error("Error in client:", err));
