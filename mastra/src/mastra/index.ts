fef7e02098124ce19e1d3eb300bb36784ebb8089
Giga Mania <hil.giga@gmail.com>
2025-09-23 22:35:11 +0100

    mastra thoughts

--- mastra/src/mastra/index.ts
+++ mastra/src/mastra/index.ts
@@ -1,11 +1,10 @@
 import { Mastra } from '@mastra/core/mastra';
 import { PinoLogger } from '@mastra/loggers';
 import { LibSQLStore } from '@mastra/libsql';
-import { weatherAgent } from './agents/weather-agent';
 import { fsAgent } from './agents/fs-agent';
 
 export const mastra = new Mastra({
-  agents: { weatherAgent, fsAgent },
+  agents: { fsAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
---
+++ src/19-mastra-way.ts
@@ -0,0 +1,7 @@
+// refer to /mastra
+
+// does loads of things out of the box lol
+// ready to go setup, just focus on the product, gettings things done.
+// creates Mastra instance, accepts agents with tools, workflows, can have/keep memory (own sql store),loggers, ready to go server.
+
+// comes with nice ui and playground.
